f8d06bd62c90387f5f32769c85ff63db3fd01b12
Kevelyn Lin <kevelynlin1224@gmail.com>
2026-03-07 21:29:05 -0800

    Update used test code

--- test_code/test_fifo.s
+++ test_code/test_fifo.s
@@ -1,44 +1,81 @@
 .text
     .global _start
 _start:
-    @ 1. Hijack the Network FIFO
+    @ 1. Construct 0x80001000 in R0
     MOV R0, #0x80
+    NOP
+    NOP
+    NOP
     LSL R0, R0, #24
+    NOP
+    NOP
+    NOP
     ADD R0, R0, #0x1000
     MOV R1, #1
+    NOP
+    NOP
+    NOP
     STR R1, [R0]
 
-    @ 2. Polling Loop
+    @ 2. Polling Loop Setup
     ADD R2, R0, #0x04
+    NOP
+    NOP
+    NOP
 wait_loop:
-    LDR R3, [R2]         @ Read Status Register
-    CMP R3, #1           @ Is a packet ready?
-    BEQ process_packet   @ If YES, jump out of the loop!
-    B wait_loop          @ If NO, keep waiting.
+    LDR R3, [R2]
+    NOP
+    NOP
+    NOP
+    CMP R3, #1
+    NOP
+    NOP
+    NOP
+    BEQ process_packet
+    NOP
+    NOP
+    NOP
+    B wait_loop
 
 process_packet:
-    @ 3. Read the Network Packet Payload
     MOV R4, #0x80
-    LSL R4, R4, #24      
-    LDR R5, [R4]         
-
-    @ 4. Feed Payload to GPU
     MOV R7, #0x81
-    LSL R7, R7, #24      
-    STR R5, [R7]         
-
-    @ 5. Trigger GPU
+    NOP
+    NOP
+    NOP
+    LSL R4, R4, #24
+    LSL R7, R7, #24
+    NOP
+    NOP
+    NOP
+    LDR R5, [R4]     @ Read Payload
+    NOP
+    NOP
+    NOP
+    STR R5, [R7]     @ Write Payload to GPU
+    
+    @ Trigger GPU
     ADD R8, R7, #0x1000
     MOV R10, #1
-    STR R10, [R8]        
+    NOP
+    NOP
+    NOP
+    STR R10, [R8]
 
-    @ 6. Read Result & Overwrite Network Payload
-    LDR R11, [R7]        
+    @ Read Result and Overwrite Payload
+    LDR R11, [R7]
+    NOP
+    NOP
+    NOP
     STR R11, [R4]
 
-    @ 7. Release FIFO
+    @ Release FIFO
     MOV R1, #0
+    NOP
+    NOP
+    NOP
     STR R1, [R0]
 
 end_loop:
     B end_loop
+	
